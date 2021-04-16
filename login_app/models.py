39e62f3e6e4d5336af3ad1135cfc87e8149951ee
Saaransh678 <45371387+Saaransh678@users.noreply.github.com>
2021-04-16 20:30:28 +0530

    Update models.py

--- login_app/models.py
+++ login_app/models.py
@@ -1,3 +1,38 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Faculty(models.Model):
+    ID=models.CharField(max_length=122)
+    name=models.CharField(max_length=122)
+    dept=models.CharField(max_length=122)
+    leaves_remaining=models.IntegerField()
+class CrossCutting(models.Model):
+    ID=models.CharField(max_length=122)
+    FacultyID=models.CharField(max_length=122)
+    name=models.CharField(max_length=122)
+    Permission_level=models.IntegerField()
+    Designation=models.CharField(max_length=122)
+class Previous_Cross_Cutting(models.Model):
+    FacultyID=models.CharField(max_length=122)
+    Timebegin=models.DateField()
+    Timeend=models.DateField()
+    Permission=models.IntegerField()
+class activeleaveentries(models.Model):
+    entryid=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    FacultyID=models.CharField(max_length=122)
+    application_date=models.DateField()
+    Starting_date=models.DateField()
+    No_of_leaves=models.IntegerField()
+    current_status=models.IntegerField()
+    comment=models.TextField()
+class comments(models.Model):
+    entryid=models.UUIDField()
+    body=models.TextField()
+    fromfacultyid=models.CharField(max_length=122)
+class www(models.Model):
+    entryid=models.UUIDField()
+    FacultyID=models.CharField(max_length=122)
+
+
+
