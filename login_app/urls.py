1d5fc63ad9b2900f181c3c94de913f449a88972e
Saaransh678 <45371387+Saaransh678@users.noreply.github.com>
2021-04-17 16:21:37 +0530

    Add files via upload

--- login_app/urls.py
+++ login_app/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.login_req)
+    path('', views.index),
+    path('login/',views.login_req,name="login"),
+    path('logout/',views.logoutuser,name="logout"),
+     path('profile/',views.profile,name="profile"),
+     path('application/',views.application,name="application"),
+    path('status/',views.status,name="status"),
 ]
